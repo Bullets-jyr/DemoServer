82afd8fa73716f06d2ee013e8f44e1df625624c2
Bullets-jyr <jyr@bullets.co.kr>
2023-07-30 16:37:33 +0900

    5. Create Ktor Project Manually

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
+    application
     kotlin("jvm") version "1.5.10"
 }
 
 group = "kr.co.bullets"
 version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
 
 repositories {
     mavenCentral()
@@ -11,4 +16,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("io.ktor:ktor-server-core:1.6.4")
+    implementation("io.ktor:ktor-server-netty:1.6.4")
+    implementation("ch.qos.logback:logback-classic:1.2.5")
 }
