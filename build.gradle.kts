858ac2e1ab075aa2366447719bca8b60361d46c6
Bullets-jyr <jyr@bullets.co.kr>
2023-07-30 14:03:16 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "kr.co.bullets"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
