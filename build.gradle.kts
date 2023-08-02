08304d47d46bbf82b0b2880fddd79d6bfc71f6e5
Bullets-jyr <jyr@bullets.co.kr>
2023-08-02 14:26:53 +0900

    11. Requests/Responses

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.5.10"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 group = "kr.co.bullets"
@@ -20,4 +21,5 @@ dependencies {
     implementation("io.ktor:ktor-server-core:1.6.4")
     implementation("io.ktor:ktor-server-netty:1.6.4")
     implementation("ch.qos.logback:logback-classic:1.2.5")
+    implementation("io.ktor:ktor-serialization:1.6.4")
 }
--- src/main/kotlin/kr/co/bullets/Application.kt
+++ src/main/kotlin/kr/co/bullets/Application.kt
@@ -1,13 +1,21 @@
 package kr.co.bullets
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.Serializable
+import kotlin.Exception
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            json()
+        }
         module()
 //        module2()
     }.start(wait = true)
@@ -20,8 +28,41 @@ fun Application.module() {
 //        }
 //    }
     install(Routing) {
+//        route(path = "/", method = HttpMethod.Get) {
+//           handle {
+//               call.respondText("Hello, World!")
+//           }
+//        }
         get("/") {
             call.respondText("Hello, World!")
+        }
+        get("/users/{username}") {
+            val username = call.parameters["username"]
+            val header = call.request.headers["Connection"]
+            if (username == "Admin") {
+                call.response.header(name = "CustomHeader", "Admin")
+                call.respond(message = "Hello Admin", status = HttpStatusCode.OK)
+            }
+            call.respondText("Greetings, $username with header: $header")
+        }
+        get("/user") {
+            val name = call.request.queryParameters["name"]
+            val age = call.request.queryParameters["age"]
+            call.respondText("Hi, my name is $name, I'm $age years old!")
+        }
+        get("/person") {
+            val person = Person(name = "jyr", age = 26)
+            try {
+                call.respond(message = person, status = HttpStatusCode.OK)
+            } catch (e: Exception) {
+                call.respond(message = "${e.message}", status = HttpStatusCode.BadRequest)
+            }
+        }
+        get("/redirect") {
+            call.respondRedirect("/moved", permanent = false)
+        }
+        get("/moved") {
+            call.respondText("You have been successfully redirected!")
         }
     }
 }
@@ -33,4 +74,10 @@ fun Application.module() {
 //        }
 //    }
 //}
+
+@Serializable
+data class Person(
+    val name: String,
+    val age: Int,
+)
 
